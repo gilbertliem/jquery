c33056bd1093c54e85626e398ed3c8882b5999fb
Gilbert Yohanes <yohanesbert97@gmail.com>
2020-09-09 09:04:30 +0700

    update jquery

--- script.js
+++ script.js
@@ -1,57 +1,60 @@
-const input = document.getElementById('input'),
-      buttonadd = document.getElementById('add'),
-      list = document.getElementById('lists'),
-      buttonsort = document.getElementById('sort');
+// const input = document.getElementById('input'),
+//       buttonadd = document.getElementById('add'),
+//       list = document.getElementById('lists'),
+//       buttonsort = document.getElementById('sort');
 
-let todo = [];
+// let todo = [];
 
-buttonadd.addEventListener('click', add);
+// buttonadd.addEventListener('click', add);
 
-if(todo.length){
-    show();
-}
+// if(todo.length){
+//     show();
+// }
 
-function show(){
-    list.innerHTML = "";
-    for (let i = 0; i < todo.length; i++) {
-        todo[i].date = new Date(todo[i].date);
-        list.innerHTML += `<li>${todo[i].text} | ${todo[i].date}${todo[i].completed}</li>`
-    }
-}
-function add(){
-    let val = input.value;
-    todo.push({
-        text: val,
-        date: new Date(),
-        completed: false,
-    })
-    show();
-}
+// function show(){
+//     list.innerHTML = "";
+//     for (let i = 0; i < todo.length; i++) {
+//         todo[i].date = new Date(todo[i].date);
+//         list.innerHTML += `<li>${todo[i].text} | ${todo[i].date}${todo[i].completed}</li>`
+//     }
+// }
+// function add(){
+//     let val = input.value;
+//     todo.push({
+//         text: val,
+//         date: new Date(),
+//         completed: false,
+//     })
+//     show();
+// }
 
 // change all of this to jquery code
 
-const input = $('#input'),
+const input = $('#input'), // get element
       buttonadd = $('#add'),
       list = $('#lists'),
       buttonsort = $('#sort');
 
 let todo = [];
 
-buttonadd.click();
+// buttonadd.addEventListener('click', add);
+buttonadd.click(add);
 
 if(todo.length){
     show();
 }
 
 function show(){
-    list.innerHTML = "";
+    list.html(""); // change to html ('') 
+    let content = ""; // create empty content
     for (let i = 0; i < todo.length; i++) {
         todo[i].date = new Date(todo[i].date);
-        list.innerHTML += `<li>${todo[i].text} | ${todo[i].date}${todo[i].completed}</li>`
+        content += `<li>${todo[i].text} | ${todo[i].date}${todo[i].completed}</li>`
+        list.html(content); // add to content
     }
 }
 function add(){
-    let val = input.value;
+    let val = input.val();
     todo.push({
         text: val,
         date: new Date(),
