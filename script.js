15fe651df5d9e600a85d88079f9f089eb7e0865d
Gilbert Yohanes <yohanesbert97@gmail.com>
2020-09-09 08:09:52 +0700

    update jquery

--- script.js
+++ script.js
@@ -29,3 +29,33 @@ function add(){
 }
 
 // change all of this to jquery code
+
+const input = $('#input'),
+      buttonadd = $('#add'),
+      list = $('#lists'),
+      buttonsort = $('#sort');
+
+let todo = [];
+
+buttonadd.click();
+
+if(todo.length){
+    show();
+}
+
+function show(){
+    list.innerHTML = "";
+    for (let i = 0; i < todo.length; i++) {
+        todo[i].date = new Date(todo[i].date);
+        list.innerHTML += `<li>${todo[i].text} | ${todo[i].date}${todo[i].completed}</li>`
+    }
+}
+function add(){
+    let val = input.value;
+    todo.push({
+        text: val,
+        date: new Date(),
+        completed: false,
+    })
+    show();
+}
